72ab91c2ac6144bb730acceb2d236d1b4fe811aa
Hui Ren <hren@athenahealth.com>
2021-11-03 14:53:08 -0500

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=hwdgrmy/api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag api $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
