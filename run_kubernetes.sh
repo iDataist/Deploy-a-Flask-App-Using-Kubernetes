d65484e5567c867ea593c691cbc11b96c1188838
Hui Ren <hren@athenahealth.com>
2021-11-03 14:53:04 -0500

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=hwdgrmy/api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run --image=$dockerpath apipod --labels app=api
 
+# kubectl create deployment --image=python python-app
+# kubectl run --image=$dockerpath python-app --labels app=api
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward apipod 8000:80
